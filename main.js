cabca9136ca2d7357e4d47a8aaee3282bc251de0
mohammadborabadi0 <mohammadborabadi0@gmail.com>
2022-05-21 10:11:25 -0700

    add Services Section to Project

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ menuIcon.addEventListener("click", function () {
 });
 
 // End Navbar
+
+// Services
+
+let tabButtons = document.querySelectorAll(".tab");
+let tabPanes = document.querySelectorAll(".tab-pane");
+
+tabButtons.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabPanes.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabButtons.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabButtons[index].classList.add("active");
+    tabPanes[index].classList.add("active");
+  });
+});
+
+// End Services
