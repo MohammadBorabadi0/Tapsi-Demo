b809064e3e23df691be01b4853a4f130595b8695
mohammadborabadi0 <mohammadborabadi0@gmail.com>
2022-05-22 04:50:19 -0700

    add Accordion Section to Project

--- main.js
+++ main.js
@@ -29,3 +29,27 @@ tabButtons.forEach((tab, index) => {
 });
 
 // End Services
+
+// Accordion
+
+let accordionTilte = document.querySelectorAll(".accordion__button");
+let accordionContent = document.querySelectorAll(".accordion__content");
+let accordionIcons = document.querySelectorAll(".accordion .icon");
+
+accordionTilte.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+    accordionContent[index].classList.toggle("active");
+    accordionIcons[index].classList.replace("fa-plus", "fa-minus");
+
+    accordionContent.forEach((content, contentIndex) => {
+      if (contentIndex !== index) {
+        accordionTilte[contentIndex].classList.remove("active");
+        accordionContent[contentIndex].classList.remove("active");
+        accordionIcons[contentIndex].classList.replace("fa-minus", "fa-plus");
+      }
+    });
+  });
+});
+
+// End Accordion 
